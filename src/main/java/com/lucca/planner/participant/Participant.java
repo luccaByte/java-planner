6a61c586a83be76a8abf4f2cb87be22c34334b9b
lucas <lucaston2323@gmail.com>
2024-07-09 14:20:18 -0300

    feat: entidade participant

---
+++ src/main/java/com/lucca/planner/participant/Participant.java
@@ -0,0 +1,43 @@
+package com.lucca.planner.participant;
+
+import java.util.UUID;
+
+import com.lucca.planner.trip.Trip;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column( nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
